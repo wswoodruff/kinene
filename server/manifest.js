1b8460a669d2715e9320714d4dfa980fc301c6d1
wswoodruff <williamswoodruff@gmail.com>
2020-05-31 17:38:27 -0400

    Init commit :smile:

---
+++ server/manifest.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const Dotenv = require('dotenv');
+const Confidence = require('confidence');
+const Toys = require('toys');
+
+// Pull .env into process.env
+Dotenv.config({ path: `${__dirname}/.env` });
+
+// Glue manifest as a confidence store
+module.exports = new Confidence.Store({
+    server: {
+        host: 'localhost',
+        port: {
+            $env: 'PORT',
+            $coerce: 'number',
+            $default: 3000
+        },
+        debug: {
+            $filter: { $env: 'NODE_ENV' },
+            $default: {
+                log: ['error'],
+                request: ['error']
+            },
+            production: {
+                request: ['implementation']
+            }
+        }
+    },
+    register: {
+        plugins: [
+            {
+                plugin: '../lib', // Main plugin
+                options: {
+                    logRootPath: process.env.LOG_ROOT_PATH,
+                    testMode: process.env.NODE_ENV !== 'production',
+                    kinesis: {
+                        region: process.env.AWS_REGION,
+                        streamName: process.env.AWS_KINESIS_STREAM_NAME,
+                        accessKeyId: process.env.AWS_KINESIS_ACCESS_KEY_ID,
+                        secretAccessKey: process.env.AWS_KINESIS_SECRET_ACCESS_KEY
+                    }
+                }
+            },
+            {
+                plugin: {
+                    $filter: { $env: 'NODE_ENV' },
+                    $default: 'hpal-debug',
+                    production: Toys.noop
+                }
+            }
+        ]
+    }
+});
